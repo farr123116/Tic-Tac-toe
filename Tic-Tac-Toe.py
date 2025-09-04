bd15e5939e98b0a71ae4dc6e7ba02abbe6195d60
farr12345 <c4051400@newcastle.ac.uk>
2025-09-04 18:18:38 +0100

    Started by making the board and having an insert and checking if a space is free or not

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,9 +1,9 @@
 board = [" " for x in range(10)]
 # board is now: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 def insertLetter(letter, pos):
-    #adds the chosen letter to the chosen position
+    #Adds the chosen letter to the chosen position
     board[pos] = letter
 
 def spaceIsFree(pos):
-    #returns true if the position on the board is empty and false if it isnt
+    #returns true if the position on the board is empty and false if it isn't
     return board[pos] == " "
