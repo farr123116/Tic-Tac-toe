41854c4f89f57e66dc8915bb62d1e8721d166ba4
farr12345 <c4051400@newcastle.ac.uk>
2026-02-19 16:52:49 +0000

    reset the app and added the base of the program

--- Tic-Tac-Toe.py
+++
@@ -1,9 +0,0 @@
-board = [" " for x in range(10)]
-# board is now: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-def insertLetter(letter, pos):
-    #Adds the chosen letter to the chosen position
-    board[pos] = letter
-
-def spaceIsFree(pos):
-    #returns true if the position on the board is empty and false if it isn't
-    return board[pos] == " "
