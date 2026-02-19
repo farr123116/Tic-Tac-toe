57c1d2f417f3a013924b3e9d252433fe05556c2f
farr12345 <c4051400@newcastle.ac.uk>
2026-02-19 17:38:37 +0000

    changed the index and app file

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Flask is running!"
+
+@app.route("/local")
+def local():
+    return render_template("local.html")
+
+@app.route("/online")
+def online():
+    return "<h2>Online mode coming soon</h2>"
 
 if __name__ == "__main__":
     app.run(debug=True)
